1b3bc24604ff2cfbd6489ae93eef29ad752e7437
u-masao <4973920+u-masao@users.noreply.github.com>
2021-10-31 22:05:06 +0900

    formatting

--- sound-effector/src/examples/basic_effects.py
+++ sound-effector/src/examples/basic_effects.py
@@ -1,29 +1,30 @@
 import soundfile as sf
-
 from pedalboard import (
-    Pedalboard,
-    Convolution,
-    Compressor,
     Chorus,
+    Compressor,
     Gain,
-    Reverb,
-    Limiter,
     LadderFilter,
+    Limiter,
+    Pedalboard,
     Phaser,
+    Reverb,
 )
 
-audio, sample_rate = sf.read('data/raw/record.wav')
+audio, sample_rate = sf.read("data/raw/record.wav")
 
 # Make a Pedalboard object, containing multiple plugins:
-board = Pedalboard([
-    Compressor(threshold_db=-50, ratio=25),
-    Gain(gain_db=30),
-    Chorus(),
-    LadderFilter(mode=LadderFilter.Mode.HPF12, cutoff_hz=900),
-    Phaser(),
-    ## Convolution("./guitar_amp.wav", 1.0),
-    Reverb(room_size=0.25),
-], sample_rate=sample_rate)
+board = Pedalboard(
+    [
+        Compressor(threshold_db=-50, ratio=25),
+        Gain(gain_db=30),
+        Chorus(),
+        LadderFilter(mode=LadderFilter.Mode.HPF12, cutoff_hz=900),
+        Phaser(),
+        # Convolution("./guitar_amp.wav", 1.0),
+        Reverb(room_size=0.25),
+    ],
+    sample_rate=sample_rate,
+)
 
 # Pedalboard objects behave like lists, so you can add plugins:
 board.append(Compressor(threshold_db=-25, ratio=10))
@@ -34,6 +35,10 @@ board.append(Limiter())
 effected = board(audio)
 
 # Write the audio back as a wav file:
-with sf.SoundFile('data/interim/processed-output-stereo.wav', 'w', samplerate=sample_rate, channels=len(effected.shape)) as f:
+with sf.SoundFile(
+    "data/interim/processed-output-stereo.wav",
+    "w",
+    samplerate=sample_rate,
+    channels=len(effected.shape),
+) as f:
     f.write(effected)
-
