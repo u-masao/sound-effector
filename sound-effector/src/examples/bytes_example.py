33959d5af788d95f0d177e4b87e6d04408196281
u-masao <4973920+u-masao@users.noreply.github.com>
2021-11-02 00:01:17 +0900

    parse signed int

--- sound-effector/src/examples/bytes_example.py
+++ sound-effector/src/examples/bytes_example.py
@@ -1,12 +1,13 @@
-
-frame = b'\xff\xff\xff\xffXabccc'
+frame = b"\xff\xff\xff\xffXabccc"
 
 print(frame)
 print(len(frame))
 print(list(frame))
 print(list(frame[0:2]))
-print(int.from_bytes(frame[0:2], byteorder='little'))
-values= []
-for i in range(len(frame)//2):
-    values.append(int.from_bytes(frame[(i*2):(i*2+2)],byteorder='little'))
+print(int.from_bytes(frame[0:2], byteorder="little"))
+values = []
+for i in range(len(frame) // 2):
+    values.append(
+        int.from_bytes(frame[(i * 2) : (i * 2 + 2)], byteorder="little", signed=True)
+    )
 print(values)
